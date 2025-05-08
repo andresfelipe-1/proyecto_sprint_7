54ac6ace3e976153e351de5dcf1256ad0a69469b
Andres Felipe <afgomez652@icloud.com>
2025-05-07 21:59:17 -0500

    Elaboracin de codigo

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.header('Análisis de data de carros')
+
 car_data = pd.read_csv('vehicles_us.csv', sep=';')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
+model_button = st.button('Construir gráfico de dispersión')
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
@@ -15,3 +18,10 @@ if hist_button:  # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+if model_button:
+    st.write(
+        'Creacion de una grafica de dispersion que grafique el ano del modelo vs el precio')
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="model_year", y="price")
+    fig.show()  # crear gráfico de dispersión
