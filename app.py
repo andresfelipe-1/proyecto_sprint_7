a3739aecc29ba8b141b2b5995287577b9366b26f
Andres Felipe <afgomez652@icloud.com>
2025-11-10 20:18:44 -0500

    Retesteo luego de incluir revisión en el código para la segunda gráfica

--- app.py
+++ app.py
@@ -20,9 +20,13 @@ if hist_button:  # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 if model_button:
-    st.write(
-        'Creacion de una grafica de dispersion que grafique el año del modelo vs el precio')
     # crear un gráfico de dispersión
+    st.write(
+        'Creacion de una gráfica de dispersión que grafique el año del modelo vs el precio')
+    st.write('Datos totales:', len(car_data))
+    st.write('Valores nulos en model_year: ',
+             car_data['model_year'].isnull().sum())
+    st.write('Valores nulos en price: ', car_data['price'].isnull().sum())
     fig = px.scatter(car_data, x="model_year", y="price")
     # crear gráfico de dispersión
     st.plotly_chart(fig, use_container_width=True)
